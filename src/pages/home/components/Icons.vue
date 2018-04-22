ffd4b60d5cbc99b200df25e6576c680e39c459b7
primerlee <jxlizhibo@gmail.com>
2018-04-22 14:44:52 +0800

    add icon-banner

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,125 @@
+<template>
+	<swiper :options="swiperOption">
+		<swiper-slide v-for="(page, index) in pages" :key="index">
+			<div class="icons">
+				<div class="icon" v-for="item in page" :key="item.id">
+					<div class="icon-img">
+						<img :src="item.iconUrl" alt="">
+					</div>
+					<p>{{item.desc}}</p>
+				</div>
+			</div>
+		</swiper-slide>
+	</swiper>
+</template>
+<script>
+	export default {
+		name: 'HomeIcons',
+		data: function () {
+			return {
+				iconList: [
+					{
+						id: "0001",
+						iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+						desc: "景点门票"
+					},
+					{
+						id: "0002",
+						iconUrl: "http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png",
+						desc: "一日游"
+					},
+					{
+						id: "0003",
+						iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/b1/528a9e80403b8c02.png",
+						desc: "杭州乐园"
+					},
+					{
+						id: "0004",
+						iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+						desc: "千岛湖"
+					},
+					{
+						id: "0005",
+						iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+						desc: "西溪湿地"
+					},
+					{
+						id: "0006",
+						iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+						desc: "印象西湖"
+					},
+					{
+						id: "0007",
+						iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+						desc: "宋城千古情"
+					},
+					{
+						id: "0008",
+						iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+						desc: "漂流"
+					},
+					{
+						id: "0009",
+						iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+						desc: "湘湖"
+					},
+
+				],
+				swiperOption: {}
+			}
+		},
+		computed: {
+			pages () {
+				const pages = [];
+				this.iconList.forEach((item, index) => {
+					const page = Math.floor(index / 8);
+					if (!pages[page]){
+						pages[page]=[];
+					}
+					pages[page].push(item);
+				});
+				return pages
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+	@import "~styles/varibles.styl"
+	@import "~styles/mixins.styl"
+
+	.icons
+		height: 0
+		padding-bottom 50%
+		overflow hidden
+		.icon
+			overflow hidden
+			width 25%
+			height 0
+			float left
+			padding-bottom 25%
+			position relative
+			.icon-img
+				position absolute
+				left 0
+				top 0
+				right 0
+				bottom .44rem
+				box-sizing border-box
+				padding .1rem
+				img
+					height: 100%
+					display block
+					margin 0 auto
+			p
+				position absolute
+				left 0
+				right 0
+				bottom 0
+				height .44rem
+				line-height .44rem
+				color $darkColor
+				text-align center
+				ellipsis()
+
+</style>
